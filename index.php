6224d86ac25614bb3e534f5873370ab4e20349ce
Naresh Choudhary <51290002+nareshkb53@users.noreply.github.com>
2019-06-02 19:06:53 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,7 +1,8 @@
 
 <?php
+
 $file_name='Customers _Assignment_Coding Challenge (Upto 6 Years) (2).txt';
-$Cust_in_100km=customer_in_range($file_name);
+$Cust_in_km=customer_in_range($file_name);
 /*
 | This function will take two parameters first is the name of the file and 2nd No of KM and will return the user_id and name of the customer in an array format 
 | Step 1.:- Get the text file into string using the file_get_contents 
@@ -13,22 +14,24 @@ $Cust_in_100km=customer_in_range($file_name);
 | Step 6.:-For the sorting of the array user_id is used as a key of the array and array is sorted using the key sort function 
 | Step 7.:-Return the array within the given kilometer range 
 */
+
 function customer_in_range($file_name,$KM=100){
 	$Customer_json_data = file_get_contents($file_name);
 	$Customer_json_data=explode("\n", $Customer_json_data); 
-	$Cust_in_100km=array();//Output data(Customer with in the rage of given KM)
+	$Cust_in_km=array();//Output data(Customer with in the rage of given KM)
 	foreach ($Customer_json_data as $json_data) {
 		$Cust_data_array=json_decode($json_data,true);
 		$key=$Cust_data_array['user_id'];
 		$Cust_data_array['DistanceInKM']=distance_in_km($Cust_data_array['latitude'],$Cust_data_array['longitude']);
 		if($Cust_data_array['DistanceInKM']<=$KM){
 			unset($Cust_data_array['DistanceInKM'],$Cust_data_array['latitude'],$Cust_data_array['longitude']);
-			$Cust_in_100km[$key]=$Cust_data_array;		
+			$Cust_in_km[$key]=$Cust_data_array;		
 		}	
 	}	
-	ksort($Cust_in_100km);
-	return $Cust_in_100km;
+	ksort($Cust_in_km);
+	return $Cust_in_km;
 }
+
 /*
 | function to calculte the distance 
 | This function will take two parameter $latitude and $lnlongitude
@@ -36,14 +39,15 @@ function customer_in_range($file_name,$KM=100){
 | $Dublin_longitude is the lnlongitude of the Dublin that is -6.257664
 | This function will return the data in integer form 
 */
+
 function distance_in_km($latitude,$longitude){
 	$Dublin_latitude=53.339428;
 	$Dublin_longitude=-6.257664;
 	if ( $Dublin_latitude==$latitude && $Dublin_longitude==$longitude) {
 		return 0;
 	}else{
-	$theta=$Dublin_longitude-$longitude;
-	$dist = sin(deg2rad($Dublin_latitude)) * sin(deg2rad($latitude)) +  cos(deg2rad($Dublin_latitude)) * cos(deg2rad($latitude)) * cos(deg2rad($theta));
+		$theta=$Dublin_longitude-$longitude;
+		$dist = sin(deg2rad($Dublin_latitude)) * sin(deg2rad($latitude)) +  cos(deg2rad($Dublin_latitude)) * cos(deg2rad($latitude)) * cos(deg2rad($theta));
     	$dist = acos($dist);
     	$dist = rad2deg($dist);
     	$miles = $dist * 60 * 1.1515;
@@ -53,6 +57,7 @@ function distance_in_km($latitude,$longitude){
 }
 //Function distance_in_km ends
 ?>
+
 <!-- For the Representation of the  data   -->
 <!DOCTYPE html>
 <html>
@@ -68,7 +73,7 @@ function distance_in_km($latitude,$longitude){
 			</tr>
 		</thead>
 		<body>
-			<?php  foreach ($Cust_in_100km as  $value) :?>
+			<?php  foreach ($Cust_in_km as  $value) :?>
 				<tr>
 					<td> <?php echo $value['user_id'];?></td>
 					<td> <?php echo $value['name'];?></td>					
